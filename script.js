6f16b9230acfab784e61af2a0a691176a81baf02
Jeremiah Tatum <72218044+jtatum2101@users.noreply.github.com>
2021-04-26 17:45:09 -0500

    got it move but the enemy are not dying.

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const grid = document.querySelector('.grid')
-const resultsDisplay = document.querySelector('.results')
-let playerPostion = 230
+let playerPostion = 200
 let width = 16
 let direction = 1
-let goingRight = true
-let enemyId
 let enemiesRemoved = []
-let results = 0
-
+let enemyId
+let goingRight = true;
 
 for(let i = 0; i < 256; i++){
     const square = document.createElement('div');
@@ -19,7 +16,8 @@ const coordinates = Array.from(document.querySelectorAll('.grid div'));
 const enemies = [
     0,1,2,3,4,5,6,
     16,17,18,19,20,21,22,
-    32,33,34,35,36,37,38
+    32,33,34,35,36,37,38,
+    48,49,50,51,52,53,54
 ]
 function draw(){
     for(let i = 0; i < enemies.length; i++){
@@ -27,10 +25,11 @@ function draw(){
     }
 }
 
-
-
-
-draw()
+function remove(){
+    for(let i = 0; i < enemies.length; i++){
+        coordinates[enemies[i]].classList.remove('invader')
+    }
+}
 
 coordinates[playerPostion].classList.add('player')
 
@@ -41,7 +40,7 @@ function movePlayer(e) {
         if (playerPostion % width !== 0) playerPostion -=1
         break
       case 'd' :
-        if (playerPostion % width < width -1) playerPostion +=1
+        if (playerPostion % width < width - 1) playerPostion +=1
         break
     }
     coordinates[playerPostion].classList.add('player')
@@ -52,6 +51,7 @@ function moveInvaders() {
     const leftEdge = enemies[0] % width === 0
     const rightEdge = enemies[enemies.length -1] % width === width -1
     remove()
+  
     if (rightEdge && goingRight) {
       for (let i = 0; i < enemies.length; i++) {
         enemies[i] += width -1 
@@ -59,6 +59,7 @@ function moveInvaders() {
         goingRight = false
       }
     }
+  
     if(leftEdge && !goingRight) {
       for (let i = 0; i < enemies.length; i++) {
         enemies[i] += width +1
@@ -66,11 +67,14 @@ function moveInvaders() {
         goingRight = true
       }
     }
+  
     for (let i = 0; i < enemies.length; i++) {
       enemies[i] += direction
     }
+  
     draw()
 }
+
 enemyId = setInterval(moveInvaders, 300)
 
 
@@ -96,6 +100,7 @@ function laser(pew){
             enemiesRemoved.push(enemyRemoved)
             results++
             resultsDisplay.innerHTML = results
+            console.log(enemiesRemoved)
         } 
     }
     switch(pew.key){
