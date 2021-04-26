58b0484405671948f1a48b2f47ac39f94d3b0691
Jeremiah Tatum <72218044+jtatum2101@users.noreply.github.com>
2021-04-26 16:30:56 -0500

    fixing some stuff

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const grid = document.querySelector('.grid')
 const resultsDisplay = document.querySelector('.results')
 let playerPostion = 230
 let width = 16
+let direction = 1
+let goingRight = true
+let enemyId
 let enemiesRemoved = []
 let results = 0
 
@@ -14,9 +17,9 @@ for(let i = 0; i < 256; i++){
 const coordinates = Array.from(document.querySelectorAll('.grid div'));
 
 const enemies = [
-    0,1,2,3,4,5,6,7,8,9,
-    15,16,17,18,19,20,21,22,23,24,
-    30,31,32,33,34,35,36,37,38,39
+    0,1,2,3,4,5,6,
+    16,17,18,19,20,21,22,
+    32,33,34,35,36,37,38
 ]
 function draw(){
     for(let i = 0; i < enemies.length; i++){
@@ -25,14 +28,14 @@ function draw(){
 }
 
 
-const squares = Array.from(document.querySelectorAll('.grid div'))
+
 
 draw()
 
-squares[playerPostion].classList.add('player')
+coordinates[playerPostion].classList.add('player')
 
 function movePlayer(e) {
-    squares[playerPostion].classList.remove('player')
+    coordinates[playerPostion].classList.remove('player')
     switch(e.key) {
       case 'a':
         if (playerPostion % width !== 0) playerPostion -=1
@@ -41,25 +44,52 @@ function movePlayer(e) {
         if (playerPostion % width < width -1) playerPostion +=1
         break
     }
-    squares[playerPostion].classList.add('player')
+    coordinates[playerPostion].classList.add('player')
 }
 document.addEventListener('keydown', movePlayer)
+
+function moveInvaders() {
+    const leftEdge = enemies[0] % width === 0
+    const rightEdge = enemies[enemies.length -1] % width === width -1
+    remove()
+    if (rightEdge && goingRight) {
+      for (let i = 0; i < enemies.length; i++) {
+        enemies[i] += width -1 
+        direction = -1
+        goingRight = false
+      }
+    }
+    if(leftEdge && !goingRight) {
+      for (let i = 0; i < enemies.length; i++) {
+        enemies[i] += width +1
+        direction = 1
+        goingRight = true
+      }
+    }
+    for (let i = 0; i < enemies.length; i++) {
+      enemies[i] += direction
+    }
+    draw()
+}
+enemyId = setInterval(moveInvaders, 300)
+
+
 
 
 function laser(pew){
     let laserId
     let laserPostion = playerPostion
     function movingLaser(){
-        squares[laserPostion].classList.remove('laser')
+        coordinates[laserPostion].classList.remove('laser')
         laserPostion -= width 
-        squares[laserPostion].classList.add('laser')
+        coordinates[laserPostion].classList.add('laser')
 
-        if (squares[laserPostion].classList.contains('invader')){
-            squares[laserPostion].classList.remove('laser')
-            squares[laserPostion].classList.remove('invader')
-            squares[laserPostion].classList.add('boom')
+        if (coordinates[laserPostion].classList.contains('invader')){
+            coordinates[laserPostion].classList.remove('laser')
+            coordinates[laserPostion].classList.remove('invader')
+            coordinates[laserPostion].classList.add('boom')
 
-            setTimeout(()=> squares[laserPostion].classList.remove('boom'), 300)
+            setTimeout(()=> coordinates[laserPostion].classList.remove('boom'), 300)
             clearInterval(laserId)
 
             const enemyRemoved = enemies.indexOf(laserPostion)
